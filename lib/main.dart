c3ea6904303bfed038f428a4d4758909fb5b8186
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2020-11-15 10:25:24 +0530

    Bottom_Navigation_Bar_1_added

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bottom_naviation_bar/bottom_1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bottom Navifation Bar',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: BottomNavOne(),
+    );
+  }
+}
